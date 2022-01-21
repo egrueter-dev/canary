44ab6c05c9ca32c710607b583efaa74f4c994233
ocotopdf <erikgrueter@gmail.com>
2022-01-21 18:52:51 -0500

    start work on building executables for different platforms

---
+++ build-executables.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# To use this file:
+# chmod +x go-executable-build.bash to build executableA
+# ./go-executable-build.bash
+
+package=$1
+
+if [[ -z "$package" ]]; then
+  echo "usage: $0 canary"
+  exit 1
+fi
+
+package_name="canary"
+
+platforms=("windows/amd64", "linux/amd64", "darwin/amd64", "darwin/arm")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+
+    GOOS=${platform_split[0]}
+
+    GOARCH=${platform_split[1]}
+
+    output_name=$package_name'-'$GOOS'-'$GOARCH
+
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi
+
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+done
